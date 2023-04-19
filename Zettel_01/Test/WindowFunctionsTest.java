9b4dff7620d50ff3f578bd690f52c90223710588
niklas <niklasbaun@gmail.com>
2023-04-19 14:29:40 +0200

    changes to test WindowFunctionsTest

--- Zettel_01/Test/WindowFunctionsTest.java
+++ Zettel_01/Test/WindowFunctionsTest.java
@@ -10,8 +10,8 @@ class WindowFunctionsTest {
     @Test
     void slidingWindowSum() {
         int[] input = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int[] expected = {3, 6, 9, 12, 15, 18, 21, 24, 27};
-        int[] actual = WindowFunctions.slidingWindowSum(input, 3);
+        int[] expected = {15, 20, 25, 30, 35, 40, 45, 50};
+        int[] actual = WindowFunctions.slidingWindowSum(input, 5);
         assertArrayEquals(expected, actual);
 
         int[] input2 = {-5, -23, 21, 21, 67, 32, 1};
