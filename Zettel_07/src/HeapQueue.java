0b6d00fe06a5100aab290d4d97ecd62f85a3b69c
Niklas Baun <niklasbaun@gmail.com>
2023-06-02 12:52:28 +0200

    heap

--- Zettel_07/src/HeapQueue.java
+++ Zettel_07/src/HeapQueue.java
@@ -15,42 +15,76 @@ public class HeapQueue<E> implements SimpleFIFOQueue<E> {
 
     private PriorityQueue<Element<E>> priorityQueue;
 
+    /**
+     * Creates a new HeapQueue object with the given maximum capacity.
+     * @param maximumCapacity
+     */
     public HeapQueue(int maximumCapacity) {
-        //TODO
+        //check if maximumCapacity is valid
+        if (maximumCapacity < 1) {
+            throw new IllegalArgumentException("Maximum capacity must be at least 1.");
+        }
+        //create priority queue with maximumCapacity
+        this.priorityQueue = new PriorityQueue<>(maximumCapacity);
     }
 
-
+    /**
+     * Removes the first element in the queue and returns it.
+     * @return The element which, among all elements currently in this queue, was added first.
+     */
     @Override
     public E poll() {
-        // TODO
-        return null;
+        E elem = (E) this.priorityQueue.poll();
+        return elem;
     }
 
+    /**
+     * Adds the element to this queue.
+     * @param element The element to be added to this queue.
+     */
     @Override
     public void add(E element) {
-        //TODO
+        if (element == null) {
+            throw new NullPointerException("Element must not be null.");
+        }
+        this.priorityQueue.add(new Element(element, this.priorityQueue.size()-1));
     }
 
+    /**
+     * Returns the first element in the queue without removing it.
+     * @return The element which, among all elements currently in this queue, was added first.
+     */
     public E peek() {
-        //TODO
-        return null;
+        E elem = (E) this.priorityQueue.peek();
+        return elem;
     }
 
+    /**
+     * Removes all elements from this queue.
+     */
     @Override
     public void clear() {
-        //TODO
+       this.priorityQueue.clear();
     }
 
+    /**
+     * Tests whether this queue is empty.
+     * @return
+     */
     @Override
     public boolean isEmpty() {
-        //TODO
-        return false;
+
+        return this.priorityQueue.isEmpty();
     }
 
+    /**
+     * Returns the maximum capacity of this queue.
+     * @return The maximum capacity of this queue.
+     */
     @Override
     public int capacity() {
-        //TODO
-        return 0;
+
+        return this.priorityQueue.size();
     }
 
 }
--- Zettel_07/src/HeapStack.java
+++ Zettel_07/src/HeapStack.java
@@ -12,44 +12,77 @@ public class HeapStack<E> implements SimpleStack<E> {
             this.insertion = insertion;
         }
     }
-
+    
     private PriorityQueue<Element<E>> priorityQueue;
 
+    /**
+     * Creates a new HeapStack object with the given maximum capacity.
+     * @param maximumCapacity
+     */
     public HeapStack(int maximumCapacity) {
-        //TODO
+        //check if maximumCapacity is valid
+        if (maximumCapacity < 1) {
+            throw new IllegalArgumentException("Maximum capacity must be at least 1.");
+        }
+        //create priority queue with maximumCapacity
+        this.priorityQueue = new PriorityQueue<>(maximumCapacity);
     }
 
+    /**
+     * Removes the top element of this stack and returns it.
+     * @return The element on top of this stack.
+     */
     @Override
     public E pop()  {
-        //TODO
-        return null;
+        E elem = (E) this.priorityQueue.poll();
+
+        return elem;
     }
 
+    /**
+     * Pushes the element on top of this stack.
+     * @param element The element to be pushed on top of this stack.
+     */
     @Override
     public void push(E element) {
-        //TODO
+        if (element == null) {
+            throw new NullPointerException("Element must not be null.");
+        }
+        this.priorityQueue.add(new Element(element, 0));
     }
 
+    /**
+     * returns the top element of this stack without removing it.
+     * @return The element on top of this stack.
+     */
     public E peek() {
-        //TODO
-        return null;
+        E elem = (E) this.priorityQueue.peek();
+        return elem;
     }
 
+    /**
+     * Removes all elements from this stack.
+     */
     @Override
     public void clear() {
-        //TODO
+        this.priorityQueue.clear();
     }
 
+    /**
+     * Tests whether this stack is empty.
+     * @return true, if this stack does not contain any elements; false otherwise.
+     */
     @Override
     public boolean isEmpty() {
-        //TODO
-        return false;
+        return this.priorityQueue.isEmpty();
     }
-
+    /**
+     * Returns the maximum capacity of this stack.
+     * @return The maximum capacity of this stack.
+     */
     @Override
     public int capacity() {
-        //TODO
-        return 0;
+        return this.priorityQueue.size();
     }
 
 
