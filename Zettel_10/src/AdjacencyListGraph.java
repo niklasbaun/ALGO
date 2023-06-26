33425f7e0cdf6866c8d55184937179aaa5464423
niklas <niklasbaun@gmail.com>
2023-06-26 10:39:21 +0200

    added all methods for zettel 10

--- Zettel_10/src/AdjacencyListGraph.java
+++ Zettel_10/src/AdjacencyListGraph.java
@@ -58,19 +58,106 @@ public class AdjacencyListGraph<T> implements Graph<T> {
         return map.get(from).remove(to);
     }
 
-    AdjacencyMatrixGraph<T> convert() {
-        // TODO b)
+    /**
+     * method to convert tge AdjacencyListGraph to an AdjacencyMatrixGraph
+     * @return the converted AdjacencyMatrixGraph
+     */
+    AdjacencyMatrixGraph<T> convert() throws InvalidEdgeException {
+        //create new AdjacencyMatrixGraph
+        AdjacencyMatrixGraph<T> matrixGraph = new AdjacencyMatrixGraph<>();
+        //add all nodes
+        for (T node : map.keySet()) {
+            matrixGraph.addNodeElement(node);
+        }
+        //add all edges
+        for (T node : map.keySet()) {
+            for (T other : map.get(node)) {
+                matrixGraph.addEdge(node, other);
+            }
+        }
         return null;
     }
 
-    public AdjacencyListGraph<T> undirected() {
-        // TODO: c)
-        return null;
+    /**
+     * method to convert an AdjacencyListGraph to an undirected AdjacencyListGraph
+     * @return the converted undirected AdjacencyListGraph
+     */
+    public AdjacencyListGraph<T> undirected() throws InvalidEdgeException {
+        //create new AdjacencyListGraph
+        AdjacencyListGraph<T> undirectedGraph = new AdjacencyListGraph<>();
+        //add all nodes
+        for (T node : map.keySet()) {
+            undirectedGraph.addNodeElement(node);
+        }
+        //add all  existing edges
+        //for each edge in the graph, also add the edge in the other direction
+        for (T node : map.keySet()) {
+            for (T other : map.get(node)) {
+                undirectedGraph.addEdge(node, other);
+                undirectedGraph.addEdge(other, node);
+            }
+        }
+        return undirectedGraph;
     }
 
+    /**
+     * method to check if the AdjacencyListGraph is bipartit
+     * bipartit means that the graph can be colored with two colors, so that no two adjacent nodes have the same color
+     * @return true if the graph is bipartit, false if not
+     */
     public boolean undirectedGraphIsBipartit() {
-        // TODO: d)
+        //create array to store the color of each node
+        NodeColor[] colors = new NodeColor[map.size()];
+        //initialize all colors with UNCOLORED
+        Arrays.fill(colors, NodeColor.UNCOLORED);
+        //start at source node with one color
+        colors[0] = NodeColor.RED;
+        //create queue to store the nodes that have to be colored
+        Queue<T> queue = new LinkedList<>();
+        //add source node to queue
+        queue.add((T) map.keySet().toArray()[0]);
+        //while there are nodes to be colored
+        while (!queue.isEmpty()) {
+            //get next node
+            T node = queue.poll();
+            //get color of node
+            NodeColor color = colors[getIndex(node)];
+            //get complement color
+            NodeColor complementColor = complement(color);
+            //for each neighbor of the node
+            for (T neighbor : map.get(node)) {
+                //get index of neighbor
+                int index = getIndex(neighbor);
+                //if the neighbor has the same color as the node, the graph is not bipartit
+                if (colors[index] == color) {
+                    return false;
+                }
+                //if the neighbor has not been colored yet
+                if (colors[index] == NodeColor.UNCOLORED) {
+                    //color the neighbor with the complement color
+                    colors[index] = complementColor;
+                    //add the neighbor to the queue
+                    queue.add(neighbor);
+                }
+            }
+        }
         return false;
+    }
+
+    /**
+     * helper method to get the index of a node
+     * @param node
+     * @return the index of the node
+     */
+    private int getIndex(T node) {
+        int index = 0;
+        for (T other : map.keySet()) {
+            if (other.equals(node)) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
     }
 
     void print() {
--- Zettel_10/src/AdjacencyMatrixGraph.java
+++ Zettel_10/src/AdjacencyMatrixGraph.java
@@ -1,37 +1,160 @@
+import java.util.List;
+
 public class AdjacencyMatrixGraph<T> implements Graph<T> {
     public static final int INITIAL_CAPACITY = 10;
     private boolean[][] matrix;
+    //list of all nodes
+    private List<T> nodes;
 
     public AdjacencyMatrixGraph() {
         matrix = new boolean[INITIAL_CAPACITY][INITIAL_CAPACITY];
     }
 
+    /**
+     * Add a node to the graph with the given element.
+     *
+     * @param element the element which should be represented in the graph as a node
+     * @return true, if the element was added; false, if the element already existed.
+     */
     @Override
     public boolean addNodeElement(T element) {
-        // TODO a)
-        return false;
+        //check if element null
+        if(element == null){
+            return false;
+        }
+        //check if element already exists
+        for (int i = 0; i < nodes.size(); i++) {
+            if(nodes.get(i).equals(element)){
+                return false;
+            }
+        }
+        //check if matrix is full
+        if(nodes.size() == matrix.length){
+            //create new matrix with double size
+            boolean[][] newMatrix = new boolean[matrix.length*2][matrix.length*2];
+            //copy old matrix to new matrix
+            for (int i = 0; i < matrix.length; i++) {
+                System.arraycopy(matrix[i], 0, newMatrix[i], 0, matrix.length);
+            }
+            //set new matrix as matrix
+            matrix = newMatrix;
+        }
+        //add element to list
+        nodes.add(element);
+        return true;
     }
 
+    /**
+     * Remove the node with the given element from the graph.
+     *
+     * @param element the element to remove
+     * @return true, if the element was removed; false, if the element did not exist.
+     */
     @Override
     public boolean removeNodeElement(T element) {
-        // TODO a)
+        //check if element null
+        if(element == null){
+            return false;
+        }
+        //check if element exists
+        for (int i = 0; i < nodes.size(); i++) {
+            if(nodes.get(i).equals(element)){
+                //remove element from list
+                nodes.remove(i);
+                //remove all edges from and to element
+                for (int j = 0; j < matrix.length; j++) {
+                    matrix[i][j] = false;
+                    matrix[j][i] = false;
+                }
+                //close gaps in matrix
+                for (int j = i; j < matrix.length-1; j++) {
+                    for (int k = 0; k < matrix.length; k++) {
+                        matrix[j][k] = matrix[j+1][k];
+                        matrix[k][j] = matrix[k][j+1];
+                    }
+                }
+                return true;
+            }
+        }
         return false;
     }
 
+    /**
+     * add a new edge to the graph
+     *
+     * @param from the source element of the edge
+     * @param to   the destination element of the edge
+     * @throws InvalidEdgeException if one of the two elements is not part of the graph
+     */
     @Override
     public void addEdge(T from, T to) throws InvalidEdgeException {
-        // TODO a)
+        //check if elements null
+        if(from == null || to == null){
+            throw new InvalidEdgeException("One of the elements is null");
+        }
+        //check if elements exist
+        if (!nodes.contains(from)){
+            throw new InvalidEdgeException("The from element does not exist");
+        }
+        if (!nodes.contains(to)){
+            throw new InvalidEdgeException("The to element does not exist");
+        }
+        //add edge
+        matrix[nodes.indexOf(from)][nodes.indexOf(to)] = true;
+        matrix[nodes.indexOf(to)][nodes.indexOf(from)] = true;
 
     }
 
+    /**
+     * removes edge with given source and destination from graph
+     *
+     * @param from the source element of the edge
+     * @param to   the destination element of the edge
+     * @return true, if the edge was removed; false, if the edge did not exist.
+     * @throws InvalidNodeException if one of the two elements is not part of the graph
+     */
     @Override
     public boolean removeEdge(T from, T to) throws InvalidNodeException {
-        // TODO a)
-        return false;
+        //check if elements null
+        if(from == null || to == null){
+            throw new InvalidNodeException("One of the elements is null");
+        }
+        //check if elements exist
+        if (!nodes.contains(from)){
+            throw new InvalidNodeException("The from element does not exist");
+        }
+        if (!nodes.contains(to)){
+            throw new InvalidNodeException("The to element does not exist");
+        }
+        //remove edge
+        matrix[nodes.indexOf(from)][nodes.indexOf(to)] = false;
+        matrix[nodes.indexOf(to)][nodes.indexOf(from)] = false;
+        return true;
     }
 
+    /**
+     * method to convert tge AdjacencyMatrixGraph to an AdjacencyListGraph
+     * @return
+     */
     AdjacencyListGraph<T> convert() {
-        // TODO b)
-        return null;
+        //create new AdjacencyListGraph
+        AdjacencyListGraph<T> listGraph = new AdjacencyListGraph<>();
+        //add all nodes
+        for (int i = 0; i < nodes.size(); i++) {
+            listGraph.addNodeElement(nodes.get(i));
+        }
+        //add all edges
+        for (int i = 0; i < nodes.size(); i++) {
+            for (int j = 0; j < nodes.size(); j++) {
+                if(matrix[i][j]){
+                    try {
+                        listGraph.addEdge(nodes.get(i), nodes.get(j));
+                    } catch (InvalidEdgeException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return listGraph;
     }
 }
