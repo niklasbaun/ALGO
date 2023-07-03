77d061eea0b455e05fab3f58f07ddc4a4830d99b
niklas <niklasbaun@gmail.com>
2023-07-03 11:36:48 +0200

    update on zettel 12; implementation of sorting algos

--- Zettel_11/src/AdjacencyListUndirectedWeightedGraph.java
+++ Zettel_11/src/AdjacencyListUndirectedWeightedGraph.java
@@ -4,39 +4,172 @@ public class AdjacencyListUndirectedWeightedGraph<T> implements UndirectedWeight
 
     protected Map<T, List<Edge<T>>> map;
 
+    /**
+     * Constructor
+     */
     public AdjacencyListUndirectedWeightedGraph() {
         map = new HashMap<>();
     }
 
+    /**
+     * method to add an element to the graph
+     * @param element the element which should be represented in the graph as a node
+     * @return true if inserted successfully
+     */
     @Override
     public boolean addNodeElement(T element) {
-        // TODO 2.a)
-        return false;
+        //check if element is null
+        if (element == null) {
+            return false;
+        }
+        //check if element is already in the graph
+        if (map.containsKey(element)) {
+            return false;
+        }
+        //add element to the graph
+        map.put(element, new ArrayList<>());
+        return true;
     }
 
+    /**
+     * method to remove an element from the graph
+     * @param element the element to remove
+     * @return true if removed successfully
+     */
     @Override
     public boolean removeNodeElement(T element) {
-        // TODO 2.a)
-        return false;
+        //check if element is null
+        if (element == null) {
+            return false;
+        }
+        //check if element is in the graph
+        if (!map.containsKey(element)) {
+            return false;
+        }
+        //remove element from the graph
+        map.remove(element);
+        return true;
     }
 
+    /**
+     * method to add an edge between two elements
+     * @param t1     one element connected by the edge
+     * @param t2     the other element connected by the edge
+     * @param weight the weight of the new edge
+     * @throws InvalidEdgeException
+     */
     @Override
     public void addEdge(T t1, T t2, int weight) throws InvalidEdgeException {
-        // TODO 2.a)
+        //check sth is null
+        if (t1 == null || t2 == null) {
+            throw new InvalidEdgeException("One of the elements is null");
+        }
+        //check if elements are in the graph
+        if (!map.containsKey(t1) || !map.containsKey(t2)) {
+            throw new InvalidEdgeException("One of the elements is not in the graph");
+        }
+        //check if edge already exists
+        if (map.get(t1).contains(new Edge<>(t1, t2, weight))) {
+            throw new InvalidEdgeException("Edge already exists");
+        }
+        //add edge to the graph
+        map.get(t1).add(new Edge<>(t1, t2, weight));
+        map.get(t2).add(new Edge<>(t2, t1, weight));
     }
 
+    /**
+     * method to remove an edge between two elements
+     * @param t1 one element connected by the edge
+     * @param t2 the other element connected by the edge
+     * @return true if removed successfully
+     * @throws InvalidNodeException
+     */
     @Override
     public boolean removeEdge(T t1, T t2) throws InvalidNodeException {
-        // TODO 2.a)
-        return false;
+        //check sth is null
+        if (t1 == null || t2 == null) {
+            throw new InvalidNodeException("One of the elements is null");
+        }
+        //check if elements are in the graph
+        if (!map.containsKey(t1) || !map.containsKey(t2)) {
+            throw new InvalidNodeException("One of the elements is not in the graph");
+
+        }
+        //check if edge exists
+        if (!map.get(t1).contains(new Edge<>(t1, t2, 0))) {
+            return false;
+        }
+        //remove edge from the graph
+        map.get(t1).remove(new Edge<>(t1, t2, 0));
+        map.get(t2).remove(new Edge<>(t2, t1, 0));
+        return true;
     }
 
+    /**
+     * method to implement the algorithm of Kruskal and print the result
+     */
     public void kruskal() {
-        // TODO: 2.b)
+        //create a map to save the sets
+        Map<T, Set<T>> sets = new HashMap<>();
+        //create a new list of edges
+        List<Edge<T>> result = new ArrayList<>();
+        //put all edges into a PriorityQueue
+        PriorityQueue<Edge<T>> pq = new PriorityQueue<>(map.size(), Comparator.comparingInt(Edge::getWeight));
+        for (T t : map.keySet()) {
+            for (Edge<T> edge : map.get(t)) {
+                pq.add(edge);
+            }
+        }
+        //create a set for each node
+        for (T t : map.keySet()) {
+            Set<T> set = new HashSet<>();
+            set.add(t);
+            sets.put(t, set);
+        }
+        //while there are edges left
+        while (!pq.isEmpty()) {
+            //get the edge with the lowest weight
+            Edge<T> edge = pq.poll();
+            //check if the nodes are in the same set
+            if (!sets.get(edge.getT1()).equals(sets.get(edge.getT2()))) {
+                //add the edge to the result
+                result.add(edge);
+                //merge the sets
+                sets.get(edge.getT1()).addAll(sets.get(edge.getT2()));
+                //remove the second set
+                sets.remove(edge.getT2());
+            }
+        }
+        //print the result
+        for (Edge<T> edge : result) {
+            System.out.println(edge.getT1() + " - " + edge.getT2() + " : " + edge.getWeight());
+        }
     }
 
     public static void main(String[] args) throws InvalidEdgeException {
         AdjacencyListUndirectedWeightedGraph<Character> graph = new AdjacencyListUndirectedWeightedGraph<>();
-        // TODO: 2.b)
+        //add nodes
+        graph.addNodeElement('A');
+        graph.addNodeElement('B');
+        graph.addNodeElement('C');
+        graph.addNodeElement('D');
+        graph.addNodeElement('E');
+        graph.addNodeElement('F');
+        graph.addNodeElement('G');
+        graph.addNodeElement('H');
+
+        //add edges
+        graph.addEdge('A', 'B', 12);
+        graph.addEdge('A', 'C', 8);
+        graph.addEdge('A', 'G', 4);
+        graph.addEdge('B', 'G', 3);
+        graph.addEdge('C', 'G', 5);
+        graph.addEdge('C', 'D', 10);
+        graph.addEdge('D', 'F', 12);
+        graph.addEdge('D', 'H', 6);
+        graph.addEdge('F', 'H', 2);
+
+        //use Kruskal on graph
+        graph.kruskal();
     }
 }
--- Zettel_11/src/SimpleSorting.java
+++ Zettel_11/src/SimpleSorting.java
@@ -1,6 +1,10 @@
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.Math.pow;
 
 
 public class SimpleSorting {
@@ -13,8 +17,21 @@ public class SimpleSorting {
      * @return true if sorted
      */
     static <E> boolean isSorted(List<E> list, Comparator<E> comp) {
-        // Todo a)
-        return false;
+        //check if list is null
+        if (list == null) {
+            return false;
+        }
+        //check if list is empty
+        if (list.isEmpty()) {
+            return true;
+        }
+        //check if list is sorted
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (comp.compare(list.get(i), list.get(i + 1)) > 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
@@ -25,24 +42,126 @@ public class SimpleSorting {
      * @param <E>  type
      */
     static <E> void selectionSort(List<E> list, Comparator<E> comp) {
-        // Todo a)
+        //check if list is null
+        if (list == null) {
+            return;
+        }
+        //check if list is empty
+        if (list.isEmpty()) {
+            return;
+        }
+        //create a new list
+        List<E> newList = new ArrayList<>();
+
+        //for each element in the list
+        //find the smallest element
+        for (int i = 0; i < list.size(); i++) {
+            E smallest = list.get(0);
+            for (int j = 0; j < list.size(); j++) {
+                if (comp.compare(list.get(j), smallest) < 0) {
+                    smallest = list.get(j);
+                }
+            }
+            //add the smallest element to the new list
+            newList.add(smallest);
+            //remove the smallest element from the old list
+            list.remove(smallest);
+        }
+        //add all elements from the new list to the old list
+        list.addAll(newList);
     }
 
     /**
      * Sorts list with insertion sort
-     *
      * @param list list of generic objects
      * @param comp comparator
      * @param <E>  type
      */
     static <E> void insertionSort(List<E> list, Comparator<E> comp) {
-        // Todo b)
-
+        //check if list is null
+        if (list == null) {
+            return;
+        }
+        //check if list is empty
+        if (list.isEmpty()) {
+            return;
+        }
+        //create a new list
+        List<E> newList = new ArrayList<>();
+        //add the first element to the new list
+        newList.add(list.get(0));
+        //for each element in the list
+        //find the right position in the new list
+        for (int i = 1; i < list.size(); i++) {
+            E element = list.get(i);
+            //remove the element from the old list
+            list.remove(element);
+            int j = 0;
+            while (j < newList.size() && comp.compare(element, newList.get(j)) > 0) {
+                j++;
+            }
+            //add the element to the new list
+            newList.add(j, element);
+        }
+        //add all elements from the new list to the old list
+        list.addAll(newList);
     }
 
 
     public static void main(String[] args) throws Exception {
+        //test both sorting methods with different sized lists
+        for (int i = 0; i < 5; i++){
+            //create list of n random integers
+            List<Integer> unsortedlist = new ArrayList<>();
+            int n = (int) pow(10, i);
+            for (int j = 0; j < n; j++) {
+                unsortedlist.add((int) (Math.random() * n));
+            }
+            //create new list
+            List<Integer> sortedList = new ArrayList<>();
+            //add elements from the unsortedlist to the new list, but sorted
+            for (int a = 0; a < unsortedlist.size(); a++) {
+                int smallest = unsortedlist.get(0);
+                for (int b = 0; b < unsortedlist.size(); b++) {
+                    if (unsortedlist.get(b) < smallest) {
+                        smallest = unsortedlist.get(b);
+                    }
+                }
+                sortedList.add(smallest);
+                unsortedlist.remove(smallest);
+            }
+            //run both sorting methods and compare the time
+            long start = System.nanoTime();
+            selectionSort(unsortedlist, Integer::compareTo);
+            long end = System.nanoTime();
+            //convert nanoseconds to milliseconds with three decimal places
+            long time = (end - start);
+            //check if the list is sorted
+            System.out.println("Selection sort is sorted? (unsortedList): " + isSorted(unsortedlist, Integer::compareTo));
+            System.out.println("Selection sort (unsortedList): " + time + "ns");
+            start = System.nanoTime();
+            selectionSort(sortedList, Integer::compareTo);
+            end = System.nanoTime();
+            time = (end - start);
+            //check if the list is sorted
+            System.out.println("Selection sort is sorted? (sortedList): " + isSorted(sortedList, Integer::compareTo));
+            System.out.println("Selection sort (sortedList): " + time + "ns");
 
-        // Todo c)
-    }
+            start = System.nanoTime();
+            insertionSort(unsortedlist, Integer::compareTo);
+            end = System.nanoTime();
+            time = (end - start);
+            //check if the list is sorted
+            System.out.println("Insertion sort is sorted? (unsortedList): " + isSorted(unsortedlist, Integer::compareTo));
+            System.out.println("Insertion sort (unsortedList): " + time + "ns");
+            start = System.nanoTime();
+            insertionSort(sortedList, Integer::compareTo);
+            end = System.nanoTime();
+            time = (end - start);
+            //check if the list is sorted
+            System.out.println("Insertion sort is sorted? (sortedList): " + isSorted(sortedList, Integer::compareTo));
+            System.out.println("Insertion sort (sortedList): " + time + "ns");
+        }
+        }
+
 }
